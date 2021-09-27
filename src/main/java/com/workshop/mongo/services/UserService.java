dd41de7adb91d1a1d37175e411077489a684e825
DionatanLeao <dionatan.ti@outlook.com>
2021-09-27 10:57:36 -0300

    User input with POST

--- src/main/java/com/workshop/mongo/services/UserService.java
+++ src/main/java/com/workshop/mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.mongo.domain.User;
+import com.workshop.mongo.dto.UserDTO;
 import com.workshop.mongo.repository.UserRepository;
 import com.workshop.mongo.services.exception.ObjectNotFoundException;
 
@@ -29,5 +30,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repo.save(user);
+	}
+	
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 }
